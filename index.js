b959aa1ea117d9e800ccc63d4a20005e5ae5620f
Maya Nour Alah <maya.noorallah.89@gmail.com>
2019-07-04 21:41:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(destinationBlock){
+  if(destinationBlock > 42){
+    return destinationBlock - 42;
+  }
+  else
+    return 42 - destinationBlock;
+}
+
+function distanceFromHqInFeet(destinationBlock){
+  if(destinationBlock > 42){
+    return (destinationBlock - 42) * 264;
+  }
+  else
+    return (42 - destinationBlock) * 264;
+}
+
+function distanceTravelledInFeet(beginningBlock, destinationBlock){
+  if(destinationBlock > beginningBlock)
+    return (destinationBlock - beginningBlock) * 264;
+  else
+    return (beginningBlock - destinationBlock) * 264;
+}
+
+function calculatesFarePrice(beginningBlock, destinationBlock){
+  let feet = 0;
+  if(destinationBlock > beginningBlock)
+    {
+      feet = (destinationBlock - beginningBlock) * 264;
+    }
+  else
+    {
+      feet = (beginningBlock - destinationBlock) * 264;
+    }
+    
+    if(feet > 2500)
+      return 'cannot travel that far';
+    else if(feet > 2000)
+      return 25;
+    else if(feet > 400 && feet <= 2000)
+      return (feet-400) * 2 / 100;
+    else if(feet <= 400)
+      return 0;
+}
